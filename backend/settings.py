e04eb558e8f9266e4df5af85d35edf6b2875d2ab
Ikhide Augustine <augustineimoleme@gmail.com>
2024-01-16 10:32:47 +0100

    adds call report model

--- backend/settings.py
+++ backend/settings.py
@@ -10,13 +10,13 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool)
-DEBUG = True
-# DEBUG = os.environ.get('DEBUG')
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
