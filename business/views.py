01fca00b564eb5b34a68bfe2f0d5a494eb027c21
Ikhide Augustine <augustineimoleme@gmail.com>
2024-01-17 07:37:31 +0100

    Restructured the call report api

--- business/views.py
+++ business/views.py
@@ -290,14 +290,9 @@ class CallReportAPIView(APIView):
             }
 
             print(extracted_data)
-            # Assuming ResultSerializer is defined and properly configured
-            serializer = CallReportSerializer(data=extracted_data)
-
-            if serializer.is_valid():
-                serializer.save()
-                return Response(status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            # Saving the extracted data directly into the database
+            CallReport.objects.create(**extracted_data)
+            return Response(status=status.HTTP_201_CREATED)
 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
