a5fe527210357a57a6c52d7a7815fbf12276fcb7
Austin-ik <augustineimoleme@gmail.com>
2024-04-23 11:10:08 +0100

    ait callback url

---
+++ AIT/ait.py
@@ -0,0 +1,48 @@
+import requests
+
+
+def make_voice_call(username, api_key, from_number, to_numbers):
+    url = 'https://voice.africastalking.com/call'
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'apiKey': api_key
+    }
+    
+    payload = {
+        'username': username,
+        'to': ','.join(to_numbers),
+        'from': from_number,
+    }
+    try:
+        response = requests.post(url, headers=headers, data=payload)
+        if response.status_code == 201 or response.status_code == 200:
+            print("Call initiated successfully")
+            # Print the response data
+            print("Response data:", response.json()['entries'][0]['sessionId'])
+        else:
+            print("Failed to initiate call. Status code:", response.status_code)
+    except Exception as e:
+        print("Encountered an error while making the call:", str(e))
+
+
+# Example usage with your provided numbers
+username = "pmba"
+api_key = "85f8738d5a557dd54d5c54cd124b518ad568fc02c9309f402e9953c30d03504a"
+from_number = "+2347080629896"
+to_numbers = ["+2348166590317"]
+
+make_voice_call(username, api_key, from_number, to_numbers)
+
+
+
+def intro_response():
+    response = '<?xml version="1.0"?>'
+    response += '<Response>'
+    # response += '<GetDigits numDigits="1" finishOnKey="#" timeout="15" callbackUrl="http://something.com">'
+    response += '<Say voice="woman">'
+    response += "Welcome to Voice Memo. Press 1 followed by the pound sign. Press 2 followed by the pound sign to exit."
+    response += '</Say>'
+    # response += '</GetDigits>'
+    response += '</Response>'
+    return response
