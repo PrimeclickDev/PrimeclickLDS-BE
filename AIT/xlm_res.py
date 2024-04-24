44f04af7f8ff152a5b568fcc8313e5c24980483a
Austin-ik <augustineimoleme@gmail.com>
2024-04-24 06:37:08 +0100

    collect dtmf

--- AIT/xlm_res.py
+++ AIT/xlm_res.py
@@ -1,7 +1,7 @@
 def intro_response():
     response = '<?xml version="1.0"?>'
     response += '<Response>'
-    response += '<GetDigits numDigits="1" finishOnKey="#" timeout="15" callbackUrl="https://coral-app-kajof.ondigitalocean.app/call/user/input">'
+    response += '<GetDigits numDigits="1" finishOnKey="#" timeout="15" callbackUrl="https://coral-app-kajof.ondigitalocean.app/call/user/input/">'
     response += '<Say voice="man">'
     response += "Welcome to Primeclick Autoleads voice demo. Please press 1 to continue and 2 to quit."
     response += '</Say>'
