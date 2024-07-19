0c292a982172bc1a55c2a8c316bcd947df6675e2
Austin-ik <augustineimoleme@gmail.com>
2024-07-19 17:36:24 +0100

    added recording topositive and negative flow

--- AIT/xlm_res.py
+++ AIT/xlm_res.py
@@ -12,8 +12,11 @@ def intro_response(audio1):
 def positive_flow(audio2):
     response = '<?xml version="1.0"?>'
     response += '<Response>'
+    response += ('<Record finishOnKey="#" maxLength="25" trimSilence="true" playBeep="true" '
+                 'callBackUrl="https://coral-app-kajof.ondigitalocean.app/record/call/">')
     response += f'<Play url="{audio2}">'
     response += '</Play>'
+    response += '</Record>'
     response += '</Response>'
     return response
 
@@ -21,8 +24,11 @@ def positive_flow(audio2):
 def negative_flow(audio3):
     response = '<?xml version="1.0"?>'
     response += '<Response>'
+    response += ('<Record finishOnKey="#" maxLength="25" trimSilence="true" playBeep="true" '
+                 'callBackUrl="https://coral-app-kajof.ondigitalocean.app/record/call/">')
     response += f'<Play url="{audio3}">'
     response += '</Play>'
+    response += '</Record>'
     response += '</Response>'
     return response
 
