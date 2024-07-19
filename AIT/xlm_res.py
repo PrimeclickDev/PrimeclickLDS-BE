c25579b8b5dc204d198de5c67afaac0726320afb
Austin-ik <augustineimoleme@gmail.com>
2024-07-19 18:06:24 +0100

    removed recording topositive and negative flow

--- AIT/xlm_res.py
+++ AIT/xlm_res.py
@@ -12,10 +12,8 @@ def intro_response(audio1):
 def positive_flow(audio2):
     response = '<?xml version="1.0"?>'
     response += '<Response>'
-    response += '<Record finishOnKey="#" maxLength="25" trimSilence="true" playBeep="true" callBackUrl="https://coral-app-kajof.ondigitalocean.app/record/call/">'
     response += f'<Play url="{audio2}">'
     response += '</Play>'
-    response += '</Record>'
     response += '</Response>'
     return response
 
@@ -23,10 +21,8 @@ def positive_flow(audio2):
 def negative_flow(audio3):
     response = '<?xml version="1.0"?>'
     response += '<Response>'
-    response += '<Record finishOnKey="#" maxLength="25" trimSilence="true" playBeep="true" callBackUrl="https://coral-app-kajof.ondigitalocean.app/record/call/">'
     response += f'<Play url="{audio3}">'
     response += '</Play>'
-    response += '</Record>'
     response += '</Response>'
     return response
 
