713ad6641e638ae103af2f345a548716a0626cf1
Austin-ik <augustineimoleme@gmail.com>
2024-08-19 16:14:17 +0100

    UNcommented out the thank_you flow and removed the contacted status check

--- business/views.py
+++ business/views.py
@@ -414,9 +414,9 @@ class AITAPIView(APIView):
             audio_link_1 = dest_number_campaign.audio_link_1
             try:
                 xml_data = intro_response(audio_link_1)
+                return HttpResponse(xml_data, content_type='text/xml')
             except Exception as e:
                 print(e)
-            return HttpResponse(xml_data, content_type='text/xml')
         else:
             return Response({"error": "Requested campaign does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
