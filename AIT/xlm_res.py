ccb694caab6d5d492a50448f38e7c4bb2459bba6
Austin-ik <augustineimoleme@gmail.com>
2024-08-27 11:25:02 +0100

    added more threads and workers to the call launch function

--- AIT/xlm_res.py
+++ AIT/xlm_res.py
@@ -12,7 +12,7 @@ def intro_response(audio1):
 def positive_record(audio2):
     response = '<?xml version="1.0"?>'
     response += '<Response>'
-    response += '<Record finishOnKey="#" maxLength="15" trimSilence="true" playBeep="true" callbackUrl="https://coral-app-kajof.ondigitalocean.app/record/call/">'
+    response += '<Record finishOnKey="#" maxLength="25" trimSilence="true" playBeep="true" callbackUrl="https://coral-app-kajof.ondigitalocean.app/record/call/">'
     response += f'<Play url="{audio2}">'
     response += '</Play>'
     response += '</Record>'
