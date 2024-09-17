49d1422577f4bea9394579720359404b316f3473
Ikhide Augustine <augustineimoleme@gmail.com>
2024-09-17 10:39:32 +0000

    testing some ait implementation with audio again

--- AIT/xlm_res.py
+++ AIT/xlm_res.py
@@ -1,13 +1,13 @@
-# def intro_response(audio1):
-def intro_response(): #testing
+def intro_response(audio1):
+# def intro_response(): #testing
     response = '<?xml version="1.0"?>'
     response += '<Response>'
     response += '<GetDigits numDigits="1" finishOnKey="#" timeout="5" callbackUrl="https://coral-app-kajof.ondigitalocean.app/call/user/input/">'
-    # response += f'<Play url="{audio1}">'
-    # response += '</Play>'
-    response += '<Say voice="woman">'
-    response += "Welcome to Voice Memo. Press 1 followed by the pound sign. Press 2 followed by the pound sign to exit."
-    response += '</Say>'
+    response += f'<Play url="{audio1}">'
+    response += '</Play>'
+    # response += '<Say voice="woman">'
+    # response += "Welcome to Voice Memo. Press 1 followed by the pound sign. Press 2 followed by the pound sign to exit."
+    # response += '</Say>'
     response += '</GetDigits>'
     response += '</Response>'
     return response
