6b67e927b8fcc0eb3cc943cc890378a6c7bac3ff
Austin-ik <augustineimoleme@gmail.com>
2024-10-22 23:47:09 +0100

    new migration file clean up

--- business/migrations/0012_viewtimehistory_has_access_support.py
+++ business/migrations/0012_viewtimehistory_has_access_support.py
@@ -19,15 +19,4 @@ class Migration(migrations.Migration):
             name='has_access',
             field=models.BooleanField(default=True),
         ),
-        # migrations.CreateModel(
-        #     name='Support',
-        #     fields=[
-        #         ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-        #         ('email', models.CharField(blank=True, max_length=255, null=True)),
-        #         ('subject', models.CharField(blank=True, max_length=355, null=True)),
-        #         ('description', models.TextField(blank=True, null=True)),
-        #         ('resolved', models.BooleanField(default=False)),
-        #         ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='user_support', to=settings.AUTH_USER_MODEL)),
-        #     ],
-        # ),
     ]
