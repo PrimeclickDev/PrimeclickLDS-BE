b4297289c8be1722e0bf6c2140bc57e2c2514629
Austin-ik <augustineimoleme@gmail.com>
2024-10-23 10:52:11 +0100

    quickfix

--- AIT/xlm_res.py
+++ AIT/xlm_res.py
@@ -5,8 +5,8 @@ def intro_response(content1, user_name):  # Accepting content and the user's nam
 
     # Check if the content is an audio file or text
     if content1.endswith(('.mp3', '.wav', '.ogg')):
-        # If it's an audio file, just play it
-        response += f'<Play>{content1}</Play>'
+        # If it's an audio file, use the url attribute in the Play tag
+        response += f'<Play url="{content1}"/>'
     else:
         # If it's text, replace the {user_name} placeholder if present
         full_content = content1.replace("{user_name}", user_name) if "{user_name}" in content1 else content1
@@ -22,25 +22,36 @@ def positive_record(content2):
     response = '<?xml version="1.0"?>'
     response += '<Response>'
     response += '<Record finishOnKey="#" maxLength="20" trimSilence="true" playBeep="true" callbackUrl="https://coral-app-kajof.ondigitalocean.app/record/call/">'
+
+    # Check if content2 is an audio file
     if content2.endswith(('.mp3', '.wav', '.ogg')):
-        response += f'<Play>{content2}</Play>'
+        # Use the url attribute in the Play tag
+        response += f'<Play url="{content2}"/>'
     else:
+        # Otherwise, treat it as text and use Say tag
         response += f'<Say voice="man">{content2}</Say>'
+
     response += '</Record>'
     response += '</Response>'
+
     return response
 
 
 def thank_you(content3):
     response = '<?xml version="1.0" encoding="UTF-8"?>'
     response += '<Response>'
-    if content3.endswith(('.mp3', '.wav', '.ogg')):
-        response += f'<Play>{content3}</Play>'
-    else:
-        response += f'<Say voice="man">{content3}</Say>'
-    response += '</Response>'
-    return response
 
+    # Check if content3 is an audio file
+    if content3.endswith(('.mp3', '.wav', '.ogg')):
+        # Use the url attribute in the Play tag
+        response += f'<Play url="{content3}"/>'
+    else:
+        # Otherwise, treat it as text and use Say tag
+        response += f'<Say voice="man">{content3}</Say>'
+
+    response += '</Response>'
+
+    return response
 
 
 def handle_inbound(content4):
